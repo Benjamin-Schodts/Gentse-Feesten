687647a31b98fef7ff7b564b57dabdbb5abf6806
benjaminschodts <subscribe41269@gmail.com>
2016-03-25 16:31:53 +0100

    automatic commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-var hello = "hello new world";
+$(document).ready(function () {
+	var myJson;
+	$("#button1").click(function () {
+	   //$.getJSON('http://api.opencultuurdata.nl/v0/openbeelden/4558763df1b233a57f0176839dc572e9e8726a02',
+	    $.getJSON('http://www.uitinnoordholland.nl/api/agenda/plaatsen',
+
+
+	    function (data) {
+	    	myJson = data;
+	    });
+	});
+
+	$("#button2").click(function () {
+		$("#movielist").append(JSON.stringify(myJson, null, "\t"));
+		/*
+		$.each( myJson.media_urls, function( key, value ) {
+			if(value.content_type === "video/mp4"){
+				$("#movielist").append("<video src=" + value.url + " controls>Your browser does not support the video tag.</video><br>");
+			}
+		});
+*/
+	});
+});
